48bdb72c8d6a68ddc914e737c458947824015387
Rodolfo <rodolfoespinosa01@gmail.com>
2023-11-01 23:23:38 -0400

    All user routes done except for update

--- controllers/user_routes.js
+++ controllers/user_routes.js
@@ -9,6 +9,17 @@ router.get('/users', async (req, res) => {
   res.json(users);
 });
 
+// Get by user ID
+router.get('/user/:user_id', async (req, res) => {
+  const user_id = req.params.user_id;
+
+  const user = await User.findById(user_id)
+  
+
+  res.status(200).json(user);
+});
+
+// Create a user
 router.post('/user', async (req, res) => {
   try {
 const user = await User.create(req.body);
@@ -17,6 +28,20 @@ res.json(user)
   } catch(err) {
    console.log(err)
   }
-})
+});
+
+// Delete a user
+router.delete('/user/:user_id', async (req, res) => {
+  const user_id = req.params.user_id;
+  const user = await User.findByIdAndDelete(user_id); // Find the user by ID
+  if (!user) {
+    return res.status(404).json({ error: 'No user by this ID' })
+  }
+  res.status(200).json({ message: 'Delete Success' });
+});
+
+
+
+
 
 module.exports = router;
