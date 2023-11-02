bf2f57139d9c07bc9a53b2c1b9ddc5782c752da0
Rodolfo <rodolfoespinosa01@gmail.com>
2023-11-01 23:29:34 -0400

    All thought routes complete

--- controllers/thought_routes.js
+++ controllers/thought_routes.js
@@ -9,6 +9,7 @@ router.get('/thoughts', async (req, res) => {
   res.json(thoughts);
 });
 
+// Create new thought
 router.post('/thought', async (req, res) => {
   try {
 const thought = await Thought.create(req.body);
@@ -18,5 +19,14 @@ res.json(thought)
    console.log(err)
   }
 })
+
+// Get by thought ID
+router.get('/thought/:thought_id', async (req, res) => {
+  const thought_id = req.params.thought_id;
+
+  const thought = await Thought.findById(thought_id)
+
+  res.status(200).json(thought);
+});
 
 module.exports = router;
